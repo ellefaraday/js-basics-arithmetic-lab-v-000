40da6b2113e02b16fb2cb81f27df646917498e3e
Elle Faraday <faraday.elle@gmail.com>
2018-07-22 21:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = 1000000000 + oldID
 
 const ageIsValid = Number.isInteger(currentAge);
 
-const randomNumber = Math.floor(Math.random() * 10) + 9;
+const randomNumber = Math.floor(Math.random() * 20);
